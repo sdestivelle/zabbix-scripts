82f0aa9449bf73d9c2178cee3fea4fd6d51e97aa
Steve DESTIVELLE <stevedestivelle@STD.local>
2015-10-04 19:46:02 +0200

    Boxcar notifications script
    Boxcar notifications script

--- Notifications/boxcar.sh
+++ Notifications/boxcar.sh
@@ -1,36 +1,14 @@
 #!/bin/bash
 
 #       Notify by Boxcar
-#       http://www.process-one.net
+#       http://steve.destivelle.me
+# See http://help.boxcar.io/support/solutions/articles/6000004813-how-to-send-a-notification-to-boxcar-for-ios-users for sounds notification list
 
-usage() {
-        echo -e "\nUsage: $0 -a AccessToken -t MessageTitle -d MessageDetails -s SoundID"
-        echo -e "Example: -$0 -a LuckJewacat6 -t \"Some message\" -d \"Message details\" -s 21\n"
-        exit 1;
-
-}
-
-while getopts "a:t:d:s:" optionName; do
-        case "$optionName" in
-                a) token="$OPTARG" ;;
-                t) title="$OPTARG" ;;
-                d) details="$OPTARG" ;;
-                s) soundid="$OPTARG" ;;
-                \?) exit 2;;
-        esac
-done
-
-if [ -z "$token" ]; then echo -e "\nMissing AuthToken. See you Boxcar App Settings!"; usage;fi
-if [ -z "$title" ]; then echo -e "\nMissing Title. Provide some text to display in push message!"; usage;fi
-if [ -z "$details" ]; then echo -e "\nMissing MessageDetails. This is displayed in detail view of Boxcar App"; usage;fi
-if [ -z "$soundid" ]; then echo -e "\nMissing SoundID. This is integer number. Test yourself!"; usage;fi
-
-curl -d user_credentials=$token \
--d "notification[title]=Zabbix: $title" \
--d "notification[message]=$title" \
--d "notification[long_message]=$details" \
--d "notification[sound]=$soundid" \
-https://new.boxcar.io/api/notifications
+curl -d "user_credentials=${1}" \
+	-d "notification[title]=${2}" \
+	-d "notification[long_message]=${3}" \
+	-d "notification[sound]=up" \
+	https://new.boxcar.io/api/notifications
 
 echo -e
 exit 0
